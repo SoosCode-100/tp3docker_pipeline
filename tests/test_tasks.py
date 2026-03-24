28b69017a35817114fa123e97c746f320c31bec5
Soos <Salmonellaoverdose@gmail.com>
2026-03-24 17:47:11 +0100

    fixing syntax errors

--- tests/test_tasks.py
+++ tests/test_tasks.py
@@ -1,19 +1,19 @@
 import sys
 import os
 
-from app.tasks import add_task, get_tasks 
+from app import tasks
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../app")))
 
 def test_add_task():
     add_task("Learn Docker")
-    assert "Learn Docker" in add_task.tasks
+    assert "Learn Docker" in add_task.task
 
 def test_multiple_tasks():
     add_task("Lean CI")
     add_task("Learn Devops")
+    
     tasks=get_tasks()
+
     assert "Learn CI" in tasks
     assert "Learn Devops" in tasks
-
-    
